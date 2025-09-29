c141e5cd27371cc2c9573a4f626e194d8017739e
0su9 <2k7y5s2@hanmail.net>
2025-09-30 08:31:16 +0900

    Update YTE_js.js

--- YTE_js.js
+++ YTE_js.js
@@ -53,7 +53,7 @@ function generateTag() {
 <div align="center"><div class="video-wrapper"${styleAttr}>
 <iframe src="https://www.youtube.com/embed/?playlist=${videoId}&loop=1&autoplay=1&vq=highres" allow="autoplay" frameborder="0" allowfullscreen></iframe>
 </div></div>
-<link rel="stylesheet" href="yt_frm_sty.css">
+<link rel="stylesheet" href="https://0su9.github.io/css/yt_frm_sty.css">
   `.trim();
 
   resultBox.value = tag;
@@ -61,7 +61,7 @@ function generateTag() {
   const previewHTML = `
     <html>
       <head>
-        <link rel="stylesheet" href="yt_frm_sty.css">
+        <link rel="stylesheet" href="https://0su9.github.io/css/yt_frm_sty.css">
       </head>
       <body>
         <div align="center"><div class="video-wrapper"${styleAttr}>
@@ -86,3 +86,4 @@ radiusInput.addEventListener('input', generateTag);
 paddingInput.addEventListener('input', generateTag);
 
 bgcolorInput.addEventListener('input', generateTag);
+
